2e0e508e2a37c8da6bc89200bb5e9622c2369947
Yohanes Mario Chandra <yohanes.mc@gmail.com>
2016-12-17 08:10:47 +0700

    test socket application

--- main.js
+++ main.js
@@ -1,12 +1,27 @@
-var express = require('express');
-var app = express();
+// var express = require('express');
+// var app = express();
+//
+// app.set('port', (process.env.PORT || 5000));
+//
+// app.get('/', function (req, res) {
+//   res.send('Running on port: '+app.get('port'));
+// });
+//
+// app.listen(app.get('port'), function(){
+//     console.log('Running on port: '+app.get('port'));
+// });
+var net = require('net');
 
-app.set('port', (process.env.PORT || 5000));
+var port = (process.env.PORT || 5000);
 
-app.get('/', function (req, res) {
-  res.send('Running on port: '+app.get('port'));
+var server = net.createServer((socket) => {
+  socket.end('Hello world!\n');
+}).on('error', (err) => {
+  // handle errors here
+  throw err;
 });
 
-app.listen(app.get('port'), function(){
-    console.log('Running on port: '+app.get('port'));
+// grab a random port.
+server.listen(port, () => {
+  console.log('opened server on', server.address());
 });
