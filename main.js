e7d3490515d0a41dd68245520459500f322b4383
Yohanes Mario Chandra <yohanes.mc@gmail.com>
2016-12-17 09:58:53 +0700

    test pub sub

--- main.js
+++ main.js
@@ -3,16 +3,38 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.get('/', function (req, res) {
+var queues = {};
+
+app.get('/sub/:queue', function (req, res) {
     res.setHeader('Content-Type', 'text/html');
     var loop = setInterval(function(){
-        res.write("longpolling ");
+        res.write("looptest ");
         res.flushHeaders();
-    },1000);
-    setTimeout(function(){
-        clearInterval(loop);
-        res.end("endlongpolling");
-    },5000);
+    },3000);
+    if (queues[req.params.queue]==null) {
+        queues[req.params.queue] = [];
+    }
+    queues[req.params.queue].push({
+        res:res,
+        loop:loop
+    });
+});
+app.get('/pub/:queue/:data', function (req, res) {
+    if (queues[req.params.queue]==null) {
+        queues[req.params.queue] = [];
+    }
+    for (var i = 0; i < queues[req.params.queue].length; i++) {
+        queues[req.params.queue][i].res.write(JSON.stringify({
+            data:req.params.data
+        }));
+        if (req.params.data=="end") {
+            clearInterval(queues[req.params.queue][i].loop);
+            queues[req.params.queue][i].res.end();
+        }
+    }
+    res.send(JSON.stringify({
+        status:"OK"
+    }));
 });
 
 app.listen(app.get('port'), function(){
