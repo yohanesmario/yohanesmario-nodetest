ce46f7505d848de04fd05c2a442993c74a420cb4
Yohanes Mario Chandra <yohanes.mc@gmail.com>
2016-12-17 07:52:56 +0700

    change to follow heroku's way of selecting ports

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var app = express();
 
+app.set('port', (process.env.PORT || 5000));
+
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send('Running on port: '+app.get('port'));
 });
 
-app.listen(5000, function () {
-  console.log('Example app listening on port 5000!');
+app.listen(app.get('port'), function(){
+    console.log('Running on port: '+app.get('port'));
 });
